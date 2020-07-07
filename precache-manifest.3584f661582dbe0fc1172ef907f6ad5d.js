8257fa89aa1b57f100fff7320df10b192fc2eaa8
Eric Bai <ericbai@instagram.com>
2020-07-06 22:47:33 -0400

    Updates

---
+++ precache-manifest.3584f661582dbe0fc1172ef907f6ad5d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d55522dc9f811170a487e5c3925fe7",
+    "url": "/moondays/index.html"
+  },
+  {
+    "revision": "05d117e9ca47c6dddfba",
+    "url": "/moondays/static/css/2.df3db916.chunk.css"
+  },
+  {
+    "revision": "9ca47ba9f04a90ccea4a",
+    "url": "/moondays/static/css/main.b2cc7ecf.chunk.css"
+  },
+  {
+    "revision": "05d117e9ca47c6dddfba",
+    "url": "/moondays/static/js/2.5720b774.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/moondays/static/js/2.5720b774.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca47ba9f04a90ccea4a",
+    "url": "/moondays/static/js/main.cf7bb3f9.chunk.js"
+  },
+  {
+    "revision": "7546fb5ab6450dd9c7a0",
+    "url": "/moondays/static/js/runtime-main.7c09e60d.js"
+  },
+  {
+    "revision": "291461abfb305aabee7a46a868a6e46a",
+    "url": "/moondays/static/media/LEMONMILK-Medium.291461ab.woff"
+  },
+  {
+    "revision": "2d6ad9bc867b1ef712c8cb6ab3775e15",
+    "url": "/moondays/static/media/LEMONMILK-Medium.2d6ad9bc.ttf"
+  },
+  {
+    "revision": "56c1de7b9e407f6b276c4d000631ceee",
+    "url": "/moondays/static/media/LEMONMILK-Medium.56c1de7b.woff2"
+  },
+  {
+    "revision": "71dc8d52d34f9d23df6034fd50807d7c",
+    "url": "/moondays/static/media/LEMONMILK-Medium.71dc8d52.eot"
+  },
+  {
+    "revision": "794458499ceb5ab65ecd801319ccfd65",
+    "url": "/moondays/static/media/LEMONMILK-Medium.79445849.svg"
+  }
+]);
